39ad6c1b1ebc45c40a4ba159a1e2d03faba91c64
Bahdjjaa <mcrbahdja@gmail.com>
2024-12-14 23:33:05 +0100

    suite

--- GestionnaireClient/ClientRobot.h
+++ GestionnaireClient/ClientRobot.h
@@ -3,9 +3,6 @@
 
 #include "GestionnaireClient.h"
 
-// Méthode pour calculer un temps d'attente et envoyer la carte la plus petite serveur
-void jouer_carte_robot(int sockfd, Carte *cartes, int nb_cartes);
-
 //Boucle prinipale du joueur robot
 void boucle_principale_client_robot(int sockfd, const char *nom);
 
